2f45537c9978e0e3c5dcd214b4a68ad183a3f335
Chaoray <65358638+Chaoray@users.noreply.github.com>
2022-01-31 13:55:25 +0800

    Update main.py
    加了自適應大小跟壓縮比率按鍵

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ AsciiChars = [ "#", "#", "@", "%", "=", "+", "*", ":", "-", ".", " " ]
 AsciiCharsLength = len(AsciiChars) - 1
 
 # where magic started
+sizeSet = True
 consoleBufferHandle = win32console.CreateConsoleScreenBuffer(DesiredAccess = win32con.GENERIC_READ | win32con.GENERIC_WRITE, ShareMode=0, SecurityAttributes=None, Flags=1) # create console buffer
 consoleBufferHandle.SetConsoleActiveScreenBuffer() # set this handle to console buffer
-# Actually, at first I wanna use api directly, I failed, but after searching docs, I found something really interesting
 
 cap = cv2.VideoCapture(0)
 if not cap.isOpened():
@@ -24,21 +24,40 @@ while(True):
         print("Unable to receive frame. Exiting ...")
         break
 
-    #cv2.imshow('live', cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))  # uncomment it if you wanna compare the original frame
+    cv2.imshow('live', frame)  # uncomment it if you wanna compare the original frame
 
     pimg = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY))
 
     buffer = ''
-    for y in np.arange(0, pimg.height, 1 * compressionRate):
-        for x in np.arange(0, pimg.width, 1 * compressionRate):
+    for y in np.arange(0, pimg.height, compressionRate):
+        for x in np.arange(0, pimg.width, compressionRate):
            index = int((pimg.getpixel((x, y)) * AsciiCharsLength) / 255)
            buffer += AsciiChars[index]
         buffer += '\n'
     
-    consoleBufferHandle.SetConsoleCursorPosition(win32console.PyCOORDType(0, 0))
+    if not sizeSet:
+        try:
+            width = math.ceil(pimg.width / compressionRate) + 1
+            height = math.ceil(pimg.height / compressionRate) + 1
+            size = win32console.PyCOORDType(width, height)
+            consoleBufferHandle.SetConsoleScreenBufferSize(size)
+            sizeSet = True
+        except:
+            pass
+
     consoleBufferHandle.WriteConsole(buffer) # write output to console buffer
-    # boom! 3 lines of code make output fps increase a lot!
     # and if you don't understand the code I wrote, that's ok, it's not a common to print things, but it really plays an important part in windows
+
+    key = cv2.waitKey(1)
+    if key == ord('q'):
+        break
+    elif key == ord('w'):
+        compressionRate += 0.1
+        sizeSet = False
+    elif key == ord('s'):
+        compressionRate -= 0.1
+        sizeSet = False
+    
 
 cap.release()
 cv2.destroyAllWindows()
